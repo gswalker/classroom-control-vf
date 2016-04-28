79662eff714e630341081bf6059eb15e376e0d3d
gswalker <garywalker@gmail.com>
2016-04-28 14:11:44 +0100

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,7 +1,7 @@
 class users::admins {
   users::managed_users { 'joe': }
   users::managed_users { 'alice': 
-    group -> 'staff',
+    group => 'staff',
   }
   group { 'staff':
     ensure => present,
