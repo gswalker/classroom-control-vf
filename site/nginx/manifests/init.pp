63d8a14c260a125b86d8fc7feb2121de0775d041
gswalker <garywalker@gmail.com>
2016-04-28 10:22:53 +0100

    Refactored init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,42 +1,32 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   package { 'nginx':
     ensure => present,
   }
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   file { '/etc/nginx/conf.d/':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
   }
   file { '/etc/nginx/conf.d/defaults.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/default.conf',
+.    source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
   service { 'nginx':
