d9d2ae474733ff49e7800e26ea7980d59b86009e
gswalker <garywalker@gmail.com>
2016-04-28 18:35:51 +0100

    Converted to spiffy new form.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,55 @@
 class nginx {
-  File {
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+  case $::osfamily {
+    'windows' : {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html' 
+      $confdir = 'C:/ProgramData/nginx' 
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+    'debian', 'redhat': { 
+      $package = 'nginx' 
+      $owner = 'root'
+      $group = 'root' 
+      $docroot = '/var/www' 
+      $confdir = '/etc/nginx' 
+      $logdir = '/var/log/nginx'
+    }
   }
-  package { 'nginx':
+  default : {
+    fail("${module_name} is not supported under ${::osfamily}") 
+  }
+  $user = $::osfamily ? {
+    'windows' => 'nobody',
+    'debian' => 'www-data',
+    'redhat' => 'nginx',
+  }
+  File {
+    owner => $owner,
+    group => $group,
+    mode => '0664',
+  }
+  package { $package:
     ensure => present,
   }
-  file { '/var/www':
-    ensure => directory,
-  }
-  file { '/etc/nginx/nginx.conf':
+  file { "${confdir}/nginx.conf":
     ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
+    content => template('nginx/nginx.conf.erb'),
     notify => Service['nginx'],
   }
-  file { '/etc/nginx/conf.d/':
-    ensure => directory,
-  }
-  file { '/etc/nginx/conf.d/defaults.conf':
+  file { "${confdir}/conf.d/default.conf":
     ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
+    content => template('nginx/default.conf.erb'),
     notify => Service['nginx'],
   }
-  file { '/var/www/index.html':
+  file { "${docroot}/$confdir/conf.d":
+    ensure => directory,
+  }
+  file { "${docroot}/index.html":
     ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
+    content => template('nginx/nginx.conf.erb'),
+    notify => Service['nginx'],
   }
   service { 'nginx':
     ensure => running,
