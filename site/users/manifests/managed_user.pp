69afc18cfb464dc590044c01bc43c37581b31726
gswalker <garywalker@gmail.com>
2016-04-28 14:31:04 +0100

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,12 +1,28 @@
 define users::managed_user (
-  $group = $title,
+  $ensure     = 'present',
+  $managehome = true,
+  $shell      = '/bin/bash',
+  $groups     = undef,
+  $group      = $title,
+  $home       = "/home/${title}",
 ) {
   user { $title:
-    ensure => present,
+    ensure     => $ensure,
+    managehome => $managehome,
+    shell      => $shell,
+    gid        => $group,
+    groups     => $groups,
+    home       => $home,
   }
-  file { "/home/${title}":
-    ensure => directory,
-    owner => $title,
-    group => $group,
+  if $ensure == 'present' {
+    group { $group:
+      ensure => present,
+    }
+    file { "${home}/.ssh":
+      ensure => directory,
+      owner  => $title,
+      group  => $group,
+      mode   => '0750',
+    }
   }
 }
