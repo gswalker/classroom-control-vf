9af9fa49b14bc3d57e769be0864c245a88811af9
gswalker <garywalker@gmail.com>
2016-04-28 11:34:56 +0100

    Added VM check.

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,12 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} VM.":}
+  }
+  
   host { 'testing':
     ensure => 'present',
     ip => '127.0.0.1',
